3440cd7c778db4a7141b47c7cf4c1075f9692a77
boluoyanshu <lwt0621@sjtu.edu.cn>
2026-06-17 19:53:38 +0800

    Rope and Yarn

--- model/model.py
+++ model/model.py
@@ -55,3 +55,39 @@ class RMSNorm(nn.Module):
 
     def forward(self, x):
         return (self.weight * self.norm(x.float())).type_as(x)
+
+def precompute_freqs_cis(dim:int,end:int,rope_base:float=1e6,rope_scaling:dict=None):
+    freqs,atten_factor=1.0 / (rope_base ** (torch.arange(0,dim,2)[:dim//2].float()/dim)),1.0
+    if rope_scaling is not None:
+        orig_max,factor,beta_slow,beta_fast,atten_factor=(rope_scaling.get("original_max_position_embeddings",2048),rope_scaling.get("factor",16.0),
+                                                          rope_scaling.get("beta_slow",1.0),rope_scaling.get("beta_fast",32.0),rope_scaling.get("attention_factor",1.0))
+        if end > orig_max:
+            inv=lambda b: (dim * math.log(orig_max / 2 * math.pi * b ))/(2 * math.log(rope_base))
+            high = min(math.ceil(inv(beta_slow)),dim//2-1)
+            low = max(math.floor(inv(beta_fast)),0)
+            ramp=torch.clamp(torch.arange(dim//2).float()-low/(max(high-low,0.01)),0,1)
+            freqs=freqs*(1-ramp+ramp/factor)
+    t= torch.arange(end,device=freqs.device)
+    angles=torch.outer(t,freqs).float()
+    freqs_cos=torch.cat([torch.cos(angles),torch.cos(angles)],dim=-1)*atten_factor
+    freqs_sin = torch.cat([torch.sin(angles), torch.sin(angles)], dim=-1)*atten_factor
+    return freqs_cos, freqs_sin
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    def rotate_half(x):
+        x1= x[...,:x.shape[-1]//2]
+        x2= x[...,x.shape[-1]//2:]
+        return torch.cat([-x2,x1],dim=-1)
+    q_embed= q *cos.unsqueeze(unsqueeze_dim)+rotate_half(q)*sin.unsqueeze(unsqueeze_dim)
+    k_embed = k * cos.unsqueeze(unsqueeze_dim) + rotate_half(k) * sin.unsqueeze(unsqueeze_dim)
+    return q_embed,k_embed
+
+q=torch.randn(2,4,2,8)
+k=torch.randn(2,4,2,8)
+cos,sin=precompute_freqs_cis(8,4)
+q_embed,k_embed=apply_rotary_pos_emb(q,k,cos,sin)
+print(q_embed.size())
+rope_scaling = {}
+cos, sin = precompute_freqs_cis(8, 4, rope_scaling=rope_scaling)
+q_embed,k_embed=apply_rotary_pos_emb(q,k,cos,sin)
+print(q_embed.size())
